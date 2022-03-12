2210a82a13a0bc96b13bc51dfa5a5fb657cf61ff
Gabriel Fernella <gabrielfernella07@hotmail.com>
2022-03-11 21:46:38 -0300

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <h1>Principal page</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
